292ce13cc56208e4296bee686cad0eaa8730ecea
Dogan <d.alk@hotmail.de>
2017-11-25 17:31:53 +0100

    insertData

---
+++ insertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO MyGuests (MatNr, LastName, FirstName, FirstName, Adr, Email)
+VALUES ('s123456','Torvalds','Linus','Virtual Village, Berlin','oli@gmail.com'),
+('s123458', 'Zuse', 'Konrad', 'Serverberg 182, Potsdam','zko@gmx.de'),
+('s123460', 'Beuth', 'Christian', 'SQL Allee, Berlin','bch@yahoo.com');
